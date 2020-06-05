fd6aa96704377fd898e604322f035f90ab833888
Anderson0017 <andeerson0017@gmail.com>
2020-06-05 18:09:13 -0300

    Register point

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,24 @@
 import express from 'express';
 import knex from './database/connection';
 
+import PointsController from './controllers/PointsController';
+
 const routes = express.Router();
 
 routes.get('/items', async (request, response) => {
   const items = await knex('items').select('*');
 
-  return response.json(items);
+  const  serializedItems = items.map(item => {
+    return {
+      id: item.id,
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    }
+  })
+
+  return response.json(serializedItems);
 })
+
+routes.post('/points', PointsController.store);
 
 export default routes;
